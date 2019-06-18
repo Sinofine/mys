8fd03b0789e4d04007a9563aec062c8e87b3f02c
Sinofine <sinofine@outlook.com>
2019-06-18 11:19:29 +0800

    chore: update gh-pages

--- src/content.ts
+++
@@ -1,7 +0,0 @@
-const source = "https://api.github.com/repos/luxunaccess/Novels/contents";
-export default fetch(source)
-    .then(_ => _.json())
-    .then((_: {
-        name: string;
-        download_url: string;
-    }[]) => (_).map(val => ({ name: val.name, data: () => fetch(val.download_url).then(_=>_.json()) })));
